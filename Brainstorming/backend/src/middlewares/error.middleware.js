dba9f150e44194e8b87f004f3e83ea239a874346
Maribel Jaramillo <jassy.jaramillo@gmail.com>
2021-04-09 03:04:47 -0500

    Add not found and error middleware

---
+++ Brainstorming/backend/src/middlewares/error.middleware.js
@@ -0,0 +1,8 @@
+module.exports = (err, req, res, next) => {
+  const httpStatus = err.status || 500; // Si el error tiene un status lo poner, si no entonces asigna error 500
+
+  return res.status(httpStatus).send({
+    status: httpStatus,
+    message: err.nessage || 'Internal server error'
+  })
+}
