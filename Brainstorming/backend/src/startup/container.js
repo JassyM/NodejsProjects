c4f540f49766ced482861571e00abf672b51a9cc
Maribel Jaramillo <jassy.jaramillo@gmail.com>
2021-04-10 02:46:57 -0500

    Add repositories

--- Brainstorming/backend/src/startup/container.js
+++ Brainstorming/backend/src/startup/container.js
@@ -17,6 +17,9 @@ const Routes = require('../routes');
 // Models
 const { User, Idea, Comment } = require('../models');
 
+// Repositories
+const { UserRepository, IdeaRepository, CommentRepository } = require('../repositories');
+
 const container = createContainer();
 
 container
@@ -43,6 +46,11 @@ container
     User: asValue(User),
     Idea: asValue(Idea),
     Comment: asValue(Comment)
+  })
+  .register({
+    UserRepository: asClass(UserRepository).singleton(),
+    IdeaRepository: asClass(IdeaRepository).singleton(),
+    CommentRepository: asClass(CommentRepository).singleton()
   });
 
 module.exports = container;
