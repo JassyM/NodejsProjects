054a0a42c86d2b0364c39fbf1db6baf11d51ac61
Maribel Jaramillo <jassy.jaramillo@gmail.com>
2021-04-14 12:49:20 -0500

    Remove console log

--- Brainstorming/backend/src/middlewares/parse-int.middleware.js
+++ Brainstorming/backend/src/middlewares/parse-int.middleware.js
@@ -1,6 +1,5 @@
 module.exports = function(req, res, next) {
   const queryStrings = req.query;
-  console.log("query: ", queryStrings);
   for(const key in queryStrings) {
     const length = queryStrings[key].length;
     const isValid = length > 20 ? false : !isNaN(parseInt(queryStrings[key]));
@@ -11,7 +10,6 @@ module.exports = function(req, res, next) {
   }
 
   req.query = queryStrings;
-  console.log("req: ", req);
 
   next(); // next() es quien le da acceso al próximo middleware de la cola de express
 }
